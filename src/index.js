20611e827f29ec8c3f76efb6ecc4f18855f3bf5f
lolu1023 <soowkw@naver.com>
2022-06-14 02:40:39 +0900

    승자 결정하기

--- src/index.js
+++ src/index.js
@@ -5,25 +5,38 @@ import './index.css';
 // 값을 표시하기 위해 Square의 render 함수에서 {/* TODO */}를 {this.props.value}로 수정해주세요.
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value : null,
-    }
-  }
   render() {
     return(
       <button 
         className='square'
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
         // 컴포넌트에서 setState를 호출하면 React는 자동으로 컴포넌트 내부의 자식 컴포넌트 역시 업데이트합니다.
       >
-      {this.state.value}
+      {this.props.value}
     </button>
     )
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // Square에 vlaue prop을 전달하기 위해 Board의 renderSquare 함수 코드를 수정해주세요.
 
 class Board extends React.Component {
@@ -31,15 +44,37 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares : Array(0).fill(null),
+      xIsNext: true,
       // Board에 생성자를 추가하고 9개의 사각형에 해당하는 9개의 null 배열을 초기 state로 설정해주세요.
     }
+  };
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i])  {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square 
+      value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'winner: ' + winner
+    }else {
+      status = 'Next Player ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
@@ -79,7 +114,6 @@ class Game extends React.Component {
     );
   }
 }
-
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
